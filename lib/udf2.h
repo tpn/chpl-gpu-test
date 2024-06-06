6295131c9ac2fd0fd18473b59342e3de73361b12
Trent Nelson <trent@trent.me>
2024-06-06 16:45:59 -0700

    Checkpoint commit.

---
+++ lib/udf2.h
@@ -0,0 +1,10 @@
+#include "stdchpl.h"
+#include "wctype.h"
+#include "ctype.h"
+extern "C" {
+void chpl__init_Foo(int64_t _ln,
+                    int32_t _fn);
+void add_int32(chpl_external_array * result,
+               chpl_external_array * a,
+               chpl_external_array * b);
+}
